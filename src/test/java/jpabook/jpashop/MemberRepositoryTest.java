2ff8ba373751edd841cefe8d4e7031ed1e5eb007
chikeem90 <chikeem90@gmail.com>
2020-11-19 00:10:41 +0900

    애플리케이션 구현 준비까지

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    // 테스트에서 쓸 경우 모두 수행된 후 rollback
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member : " + (findMember == member));
+    }
+}
